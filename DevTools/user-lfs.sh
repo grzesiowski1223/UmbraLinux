c6a7060fb2286818bcc7ec62902eb5e9fa270770
grzesiowski1223 <84310323+grzesiowski1223@users.noreply.github.com>
2025-06-15 21:40:08 +0200

    Update and rename lsf-user.sh to user-lfs.sh

--- DevTools/user-lfs.sh
+++ DevTools/user-lfs.sh
@@ -14,18 +14,3 @@ chown -v lfs $LFS/{usr{,/*},var,etc,tools}
 case $(uname -m) in
   x86_64) chown -v lfs $LFS/lib64 ;;
 esac
-if [ $? -eq 0 ]; then
-    echo -e "${GREEN}[OK]${NC} 1st process (mkdir) was done!"
-else
-    echo -e "${RED}[ERROR]${NC} Failed to create mount point!"
-    problems=$((problems + 1))
-fi
-  sleep 1s
-
-echo "Running Tests..."
-
-if [ "$problems" -eq 0 ]; then
-    echo -e "${GREEN}0 problems found ✔️${NC}"
-else
-    echo -e "${RED}$problems problem(s) found ❌${NC}"
-fi
