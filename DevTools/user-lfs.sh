9eccdc76e5d4d29087ce25b49d2f66b5c598324c
grzesiowski1223 <84310323+grzesiowski1223@users.noreply.github.com>
2025-06-15 22:18:33 +0200

    Update user-lfs.sh + Error codes and colors

--- DevTools/user-lfs.sh
+++ DevTools/user-lfs.sh
@@ -2,15 +2,24 @@
 
 #Do not run this command if the lsf default user is already created [That sole porpouse is that gentoo live usb is not persistent so it deletes the lfs user]
 
+# COLORS
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+problems=0
+
 groupadd lfs
   sleep 1s
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
   sleep 1s
-
-passwd lfs
+sudo - lfs
   sleep 1s
-  
-chown -v lfs $LFS/{usr{,/*},var,etc,tools}
-case $(uname -m) in
-  x86_64) chown -v lfs $LFS/lib64 ;;
-esac
+export LFS=/mnt/lfs
+
+if cd /home/gentoo/Downloads && bash lfs-check.sh; then
+    echo -e "${GREEN}[OK]${NC} Process was done!"
+else
+    echo -e "${RED}[ERROR]${NC} Failed to finish process. ERR-21"
+    problems=$((problems + 1))
+fi
